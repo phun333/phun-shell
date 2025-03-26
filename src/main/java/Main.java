c922a5f0a0ec7b97d607ecb6104266d13a3e9384
ali <149762185+phun333@users.noreply.github.com>
2025-03-26 13:06:49 +0300

    update shell
    adding some comment line for better learning and readability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        // TODO i can rewrite them in a different function, for easier reading when more commands are added in the future.
         String[] commands = {"exit", "echo", "type"};
             List<String> builtins = Arrays.asList(commands);
 
@@ -18,7 +19,7 @@ public class Main {
             String command = str[0];
             StringBuilder parameter = new StringBuilder();
 
-            // if parameter words length bigger than 1, add all it to string
+            // * if parameter words length bigger than 1, add all it to string
             if(str.length > 2) {
                 for(int i = 1; i < str.length; i++) {
                     if(i < str.length-1) {
@@ -57,21 +58,21 @@ public class Main {
                     }
                     break;
                 default:
-                    String fullPath = getPath(command);
+                    String fullPath = getPath(command); // * search for the executable in the system's PATH.
                     if (fullPath != null) {
                         List<String> run = new ArrayList<>();
-                        run.add(command);
+                        run.add(command); // * add only the program name to match expected argv[0] (for testcase and better readability.)
                         for (int i = 1; i < str.length; i++) {
                             run.add(str[i]);
                         }
                         ProcessBuilder pb = new ProcessBuilder(run);
-                        pb.directory(Path.of(getCommandDir(fullPath)).toFile());
-                        pb.inheritIO();
+                        pb.directory(Path.of(getCommandDir(fullPath)).toFile()); // * set the working directory to where the executable is located
+                        pb.inheritIO(); // * redirect the child process's input/output to the current shell (tdout/stderr).
                         try {
                             Process proc = pb.start();
-                            proc.waitFor();
-                        } catch (Exception e) {
-                            System.err.println("Error: " + e.getMessage());
+                            proc.waitFor(); // * wait for the process to finish before continuing
+                        } catch (Exception err) {
+                            System.err.println("Error: " + err.getMessage());
                         }
                     } else {
                         System.out.println(input + ": command not found");
@@ -83,10 +84,14 @@ public class Main {
     }
     private static String getCommandDir(String fullPath) {
         int lastSlash = fullPath.lastIndexOf('/');
-        return (lastSlash == -1) ? "." : fullPath.substring(0, lastSlash);
+        if(lastSlash == -1){
+            return "."; // * current directory
+        }else{
+            return fullPath.substring(0, lastSlash);
+        }
     }
     /*
-     * GetCommandDir extracts the directory from a full file path by finding the last '/' character; if found, it returns the substring from the beginning up to (but not including) that slash, which gives the directory path. If no '/' is present (meaning the file is in the current directory), it returns "." to indicate the current working directory.
+     * GetCommandDir extracts the directory from a full file path by finding the last '/' character, if found, it returns the substring from the beginning up to that slash, which gives the directory path. If no '/' is present (meaning the file is in the current directory), it returns "." to indicate the current working directory.
      */
 
 
